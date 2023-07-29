43f11ca9451517c0e601b4a9e3746550ddd96be6
aainka <aainka@gmail.com>
2023-07-29 22:16:24 +0900

    update

--- web/home.py
+++ web/home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import webbrowser
 import os
-import mdv
+from mdv import Navi
 import subprocess
  
 #from st_pages import Page, Section, add_page_title, show_pages
@@ -15,19 +15,24 @@ st.set_page_config(layout="wide")
 #com.mdlist(".")
 param = st.experimental_get_query_params()
 
- 
-path = "."
-if 'path' in param:
-    path = str(param['path'][0])
+nav = Navi('..')
 
-fname = path+'/'+mdv.mdfirst(path)
-if 'md' in param :
-    fname = str(param['md'][0])
-    path= os.path.dirname(fname)
- 
-mdv.mdlist(path)
-if fname != None:
-    mdv.mdview(fname)
+nav.showDir(param)
+
+
+# path = home
+# if 'path' in param:
+#     path = str(param['path'][0])
+
+# fname = path+'/'+mdv.mdfirst(path)
+# if 'md' in param :
+#     fname = str(param['md'][0])
+#     path= os.path.dirname(fname)
+
+# ## 디렉토리 목록보이기
+# mdv.mdlist(home,None)
+# if fname != None:
+#     mdv.mdview(fname)
 
 if st.sidebar.button("GIT PUSH"):
     x = subprocess.run('/usr/bin/sh ./git_push.sh', shell=True, capture_output=True, text=True)
--- web/mdv.py
+++ web/mdv.py
@@ -44,6 +44,115 @@ def redirect_url(url: str, text: str= None, color="#FD504D"):
     """ 
     return str
 
+# path는 논리적 위치 : fpath 물리적주소
+class Navi:
+    def __init__(self, home):
+        self.home = home
+
+    def parse(self, param):
+        (self.path, self.md) = ( '', None )
+   
+        if 'md' in param:
+            self.md = str(param['md'][0])
+            self.path= os.path.dirname(self.md)
+        else :
+            if 'path' in param:
+                self.path = str(param['path'][0])
+                self.md = self.getFirst(self.path)
+            else: 
+                self.md = self.getFirst()
+        if self.path == '/':
+            self.rdir = self.home
+            self.path = ''
+        else:
+            self.rdir = self.home+self.path
+        st.sidebar.write("rd= "+self.rdir)
+        
+    
+    def getFirst(self, Path=None):
+        if Path == None:
+            self.fpath = self.home
+        else:
+            self.fpath = self.home+Path
+        return self.fpath
+
+                 
+    def showDir(self, param):
+        self.parse(param)
+        st.sidebar.write("path = "+self.path)
+        st.sidebar.write("md = "+self.md)
+        (dir_list, file_list) = self.getList()
+       
+        url_all = ''
+        st.sidebar.write("DIRLIST------------------")
+        url = "?path=%s"%('')
+        url_all += redirect_url(url,'/',color="#222222")
+        st.sidebar.markdown(url_all,unsafe_allow_html=True)
+        url_all = ''
+        for x in dir_list:
+            url = "?path=%s"%( self.path+'/'+x)
+            url_all += redirect_url(url,x,color="#005522")
+        st.sidebar.markdown(url_all,unsafe_allow_html=True)
+
+        st.sidebar.write("FILE_LIST------------------")
+        url_all = ''
+        for x in file_list:
+            url = "?md=%s"%(self.path+'/'+x)
+            url_all += redirect_url(url,x )
+        st.sidebar.markdown(url_all,unsafe_allow_html=True)
+
+        mdview(self.home+self.md)
+
+    def getList(self):
+        dir_list = []
+        file_list = []
+        f_list = os.listdir(self.rdir)
+        for x in f_list:
+            if self._contains(['pycache','.git','.jpg','.png','web'],x):
+                continue
+            else:
+                if os.path.isdir(self.rdir+'/'+x):
+                    dir_list.append(x)
+                else:
+                    file_list.append(x)
+        return ( dir_list, file_list)
+
+    def _contains(self,list, name):
+        for x in list:
+            if x in name:
+                return True
+        return False
+
+     
+
+
+
+# 디버그를 위하여 현재 디렉토리 show
+
+def mdlist(home,path):
+    url_all =''
+    # st.sidebar.write("cwd = "+path)
+    ## 현재 디렉토리 표시
+    updir = path
+    count = 10
+    base = 'http://div.iptime.org:58282'
+    base = 'http://192.168.2.51:8501'
+    if path == None:
+        updir = home
+    else:
+        updir = home+'/'+path
+    while updir != home:
+        url = "%s?path=%s"%(base,updir)
+        url_all = redirect_url(url,'/'+os.path.basename(updir),color="#888888") + url_all
+        updir = os.path.dirname(updir)
+        count -=1
+        if count < 0:
+            break
+    if count < 10:
+        url = "%s?path=%s"%(base,updir)
+        url_all = redirect_url(url,"..",color="#888888") + url_all
+        st.sidebar.markdown(url_all,unsafe_allow_html=True)
+
 def mdview(filename):
     tab1, tab2 = st.tabs([filename,"editor"])
     sline = ''
@@ -75,25 +184,6 @@ def mdfirst(path):
             return x
     return None
 
-def mdlist(path):
-    url_all =''
-    # st.sidebar.write("cwd = "+path)
-    ## 현재 디렉토리 표시
-    updir = path
-    count = 10
-    base = 'http://div.iptime.org:58282'
-    base = 'http://192.168.2.51:8501'
-    while updir != '.':
-        url = "%s?path=%s"%(base,updir)
-        url_all = redirect_url(url,'/'+os.path.basename(updir),color="#888888") + url_all
-        updir = os.path.dirname(updir)
-        count -=1
-        if count < 0:
-            break
-    if count < 10:
-        url = "%s?path=%s"%(base,updir)
-        url_all = redirect_url(url,"..",color="#888888") + url_all
-        st.sidebar.markdown(url_all,unsafe_allow_html=True)
        
     
     # subdir 표시
